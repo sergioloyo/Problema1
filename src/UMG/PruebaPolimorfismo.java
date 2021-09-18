a8b57e08cfdc1550794d26bd6cb2087ab463ed77
Proyectos-java-2dosemestre <https://github.com/sergioloyo/Proyectos-java-2dosemestre.git>
2021-09-18 09:10:21 -0600

    Problema1

--- src/UMG/PruebaPolimorfismo.java
+++ src/UMG/PruebaPolimorfismo.java
@@ -1,8 +1,12 @@
 package UMG;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class PruebaPolimorfismo {
+
+
+    ArrayList<Vehiculo>Carro=new ArrayList<>();
     int opcion=0,exit=0;
 //menu
     public static void main(String[] args) {
@@ -16,6 +20,17 @@ public void menuGeneral(){
             opcion = Integer.parseInt(JOptionPane.showInputDialog("Menu \n\n 1. Ingreso de Sedan\n 2. Ingreso de Camion\n 3. Salir\n\nIngrese su opcion"));
             switch (opcion){
                 case 1:
+                    String nom;
+                    int pla;
+                    int cili;
+
+                    nom=JOptionPane.showInputDialog("Ingrese su numero de placa: ");
+                    pla= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de ejes del Vehiculo: "));
+                    cili=Integer.parseInt(JOptionPane.showInputDialog("Indique el Cilindraje de su Vehiculo: "));
+
+                            Sedan=
+
+
                 break;
                 case 2:
                     break;
